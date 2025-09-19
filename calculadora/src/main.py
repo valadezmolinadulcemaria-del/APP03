5d6a310a57e313f04ef93288c8e4d0b9efd2b1d4
valadezmolinadulcemaria-del <valadezmolinadulcemaria@cetis50cdmx.com>
2025-09-19 02:07:00 +0000

    se sube practica 003

---
+++ calculadora/src/main.py
@@ -0,0 +1,27 @@
+import flet as ft
+import math
+
+
+def main(page: ft.Page):
+    page.title= "calculadora siemple"
+    page.bgcolor=ft.Colors.RED_100
+    
+   #titulo
+   titulo=ft.Text(
+       "🥬 caculadora basica vv",
+        size=28,
+        color=ft.Colors.RED_700
+        text_align="center",
+        weight=="bold"
+   )
+   
+   #entradas
+   num1=ft.ft.TextField
+   
+   
+   
+   
+    page.add()
+
+
+ft.app(main)
